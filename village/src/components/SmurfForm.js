3bc894f7350e8a3d6e7667bc9d4f7f058d909f3e
Ian Be <ian84be@gmail.com>
2019-03-01 11:53:52 -0600

    basic functions

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -13,12 +14,14 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    let newSmurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height,
+    }
+    axios.post('http://localhost:3333/smurfs', newSmurf)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
   }
 
   handleInputChange = e => {
