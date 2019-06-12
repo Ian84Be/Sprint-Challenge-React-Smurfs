493203c90dc0c36b975ccd08356a6bf58ed8bbaa
Ian Be <ian84be@gmail.com>
2019-06-12 17:04:45 -0500

    refactor components

--- village/src/App.js
+++ village/src/App.js
@@ -7,33 +7,22 @@ import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      smurfs: [],
-    };
-  }
-  // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
-  // Notice what your map function is looping over and returning inside of Smurfs.
-  // You'll need to make sure you have the right properties on state and pass them down to props.
-
+  state = {
+    smurfs: [],
+  };
   componentDidMount() {
+    this.getSmurfs();
+  }
+
+  getSmurfs = () => {
     axios.get('http://localhost:3333/smurfs')
-      .then(res => {
-        this.setState({
-          smurfs: res.data,
-        })
-      })
-      .catch(err => console.log(err));
+    .then(res => { this.setState({smurfs: res.data}) })
+    .catch(err => console.log(err));
   }
 
   unSmurf = e => {
     axios.delete(`http://localhost:3333/smurfs/${e.target.id}`)
-    .then(res => {
-      this.setState({
-        smurfs: res.data,
-      })
-    })
+    .then(res => { this.setState({smurfs: res.data}) })
     .catch(err => console.log(err));
   }
 
@@ -50,11 +39,11 @@ class App extends Component {
       </div>
 
       <Route path="/smurf-form" render={props => (
-        <SmurfForm {...props}/>
+        <SmurfForm {...props} getSmurfs={this.getSmurfs}/>
       )}/>
         
 
-      <Route exact path="/" render={props => (
+      <Route path="/" render={props => (
         <Smurfs {...props} smurfs={this.state.smurfs} unSmurf={this.unSmurf} />
       )}/>
 
